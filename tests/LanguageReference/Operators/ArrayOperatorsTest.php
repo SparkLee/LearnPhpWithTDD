04f32e526b2948cd4f904b2853104f7b4a3d1d41
SparkLee <liweijsj@163.com>
2019-12-25 10:37:16 +0800

    Done function array union(+) operator test

---
+++ tests/LanguageReference/Operators/ArrayOperatorsTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\LanguageReference\Operators;
+
+use Tests\TestCase;
+
+/**
+ * function array operators tests
+ *
+ * @see https://www.php.net/manual/en/language.operators.array.php
+ * @author Spark Lee <liweijsj@163.com>
+ * @since  2019/12/25
+ */
+class ArrayOperatorsTest extends TestCase
+{
+    /**
+     * The + operator returns the right-hand array appended to the left-hand array;
+     * for keys that exist in both arrays, the elements from the left-hand array will
+     * be used, and the matching elements from the right-hand array will be ignored.
+     *
+     * Example | Name  | Result
+     * ------------------------------------
+     * $a + $b | Union | Union of $a and $b.
+     */
+    public function test_union()
+    {
+        // 数组相加(合并)，右边数组加在左边数组后面
+        $a = [1, 2, 3];
+        $b = ['name' => 'Spark Lee', 'age' => 18];
+        $this->assertEquals([1, 2, 3, 'name' => 'Spark Lee', 'age' => 18], $a + $b);
+
+        $a = [1, 2, 3];
+        $b = [];
+        $this->assertEquals($a, $a + $b);
+        $this->assertEquals($a, $b + $a);
+
+        // 若 key 相同，则取左边值，右边值被忽略 | 故索引数组合并不能用'+'，得用 array_merge()
+        $a = [1, 2, 3];
+        $b = ['a', 'b', 'c', 'd', 'abc'];
+        $this->assertEquals([1, 2, 3, 'd', 'abc'], $a + $b);
+
+        $a = ['name' => 'Spark Lee', 'age' => 18];
+        $b = ['name' => '李', 'fav' => 'Coding'];
+        $this->assertEquals(['name' => 'Spark Lee', 'age' => 18, 'fav' => 'Coding'], $a + $b);
+    }
+
+}
